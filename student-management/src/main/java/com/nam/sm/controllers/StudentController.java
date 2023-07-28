4faf3aa835c443bcc331cb6aef4af40da3013968
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2023-07-28 13:58:26 +0700

    Initializing and configuring the DispatcherServlet

---
+++ student-management/src/main/java/com/nam/sm/controllers/StudentController.java
@@ -0,0 +1,13 @@
+package com.nam.sm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudentController {
+	
+	@GetMapping("/showStudent")
+	public String showStudentList() {
+		return "student-list";
+	}
+}
